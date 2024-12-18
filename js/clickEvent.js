70cd40f0963ebfcdea69e1a2c2924a7ad00e60fc
wubshet <0953895891w@gmail.com>
2024-12-18 15:15:08 +0300

    first commit

---
+++ js/clickEvent.js
@@ -0,0 +1,17 @@
+function clickEvent() {
+    const home = document.getElementById('home');
+    const about = document.getElementById('about');
+    const portfolio = document.getElementById('portfolio');
+    const contact = document.getElementById('contact');
+    const sections = [home, about, portfolio, contact];
+
+    sections.forEach(section => {
+        section.addEventListener('click', () => {
+            // Remove the 'active' class from all sections
+            sections.forEach(sec => sec.classList.remove('active'));
+            // Add the 'active' class to the clicked section
+            section.classList.add('active');
+        });
+    });
+}
+clickEvent();
