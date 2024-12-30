dafc45a776aec30bd9fd19609b3701a2c4517d33
wubshet <0953895891w@gmail.com>
2024-12-30 23:20:34 +0300

    add new logo and eddited hero image and hero about text

--- images/filemanager.ai_files/latest-version.php
+++
@@ -1,4 +0,0 @@
-
-var MONSTA_LATEST_VERSION = "2.10.4";
-if (typeof monstaLatestVersionCallback === "function")
-	monstaLatestVersionCallback("2.10.4");
--- js/clickEvent.js
+++ js/clickEvent.js
@@ -16,3 +16,25 @@ function clickEvent() {
 }
 clickEvent();
 
+function clickEventreadmore() {
+    const content = document.getElementById("heroUs");
+    const viewMoreBtn = document.getElementById("readmore");
+    const herousbag = document.getElementById("herousbag");
+    if (viewMoreBtn.textContent == "read more ▼") {
+        // Expand content
+        content.style.height = `${content.scrollHeight+100}px`;
+        content.style.overflow = "visible";
+        viewMoreBtn.textContent = "View Less ▲";
+        herousbag.style.bottom = "-10rem";
+        viewMoreBtn.style.backgroundColor = "#B7BD10FF";
+    } else {
+        // Collapse content
+        content.style.height = "200px";
+        content.style.overflow = "hidden";
+        viewMoreBtn.textContent = "read more ▼";
+        viewMoreBtn.style.backgroundColor = "#009345FF";
+        herousbag.style.bottom = "-5rem";
+    }
+  }
+  
+  
